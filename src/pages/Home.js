a6e4372b9010d9b427d32ca6e0124a139a8b63bc
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2022-04-19 12:01:16 -0400

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 function Home(){
+    const navigate = useNavigate();
+    const logMeOut = () => {
+        localStorage.removeItem('user');
+        navigate("/login");
+    }
+
     return(<>
         <h1>Home</h1>
+        <button onClick={logMeOut}>Log Out</button>
     </>);
 }
 
